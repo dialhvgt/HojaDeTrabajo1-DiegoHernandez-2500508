9be24e913b4f6da125935fae5f5a7e763bc2cef8
Diego Hernandez <valdezdiegoal14@gmail.com>
2025-07-26 20:00:05 -0600

    Primer problema

---
+++ HojaDeTrabajo1-DiegoHernandez-2500508/Libro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HojaDeTrabajo1_DiegoHernandez_2500508
+{
+    internal class Libro
+    {
+        private string titulo;
+        private string autor;
+        private bool disponible;
+        private int id;
+        private string usuarioPrestamo;
+
+        public Libro(string titulo, string autor, int id)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.id = id;
+            this.disponible = true;
+            this.usuarioPrestamo = "";
+        }
+        public string ObtenerTitulo()
+        {
+            return titulo;
+        }
+
+        public bool EstaDisponible()
+        {
+            return disponible;
+        }
+
+        public void Prestar(string usuario)
+        {
+            disponible = false;
+            usuarioPrestamo = usuario;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine("ID: " + id + " | Título: " + titulo + " | Autor: " + autor + " | Disponible: " + disponible);
+        }
+    }
+}
+
--- HojaDeTrabajo1-DiegoHernandez-2500508/Program.cs
+++ HojaDeTrabajo1-DiegoHernandez-2500508/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HojaDeTrabajo1_DiegoHernandez_2500508;
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Libro> libros = new List<Libro>();
+        int siguienteId = 1;
+
+        void RegistrarLibro(string titulo, string autor)
+        {
+            foreach (Libro l in libros)
+            {
+                if (l.ObtenerTitulo() == titulo)
+                {
+                    Console.WriteLine("Ese libro ya está registrado.");
+                    return;
+                }
+            }
+
+            Libro nuevoLibro = new Libro(titulo, autor, siguienteId);
+            libros.Add(nuevoLibro);
+            siguienteId++;
+            Console.WriteLine("Libro registrado correctamente.");
+        }
+
+        void PrestarLibro(string titulo, string usuario)
+        {
+            foreach (Libro l in libros)
+            {
+                if (l.ObtenerTitulo() == titulo)
+                {
+                    if (l.EstaDisponible())
+                    {
+                        l.Prestar(usuario);
+                        Console.WriteLine("Libro prestado a " + usuario);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El libro no está disponible.");
+                    }
+                    return;
+                }
+            }
+
+            Console.WriteLine("Libro no encontrado.");
+        }
+
+        void MostrarLibrosDisponibles()
+        {
+            Console.WriteLine("\nLibros disponibles:");
+            foreach (Libro l in libros)
+            {
+                if (l.EstaDisponible())
+                {
+                    l.MostrarInfo();
+                }
+            }
+        }
+
+        RegistrarLibro("El Principito", "Antoine de Saint-Exupéry");
+        RegistrarLibro("Cien años de soledad", "Gabriel García Márquez");
+
+        MostrarLibrosDisponibles();
+
+        PrestarLibro("El Principito", "Ana");
+
+        MostrarLibrosDisponibles();
+    }
+}
